798d693486f5abc796972d0f64e2306bb0faa694
cychann <skcbdlhc129@naver.com>
2021-05-21 10:59:37 +0900

    body design

--- food_store/lib/main.dart
+++ food_store/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Food Store',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -21,14 +20,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  int _selectedScore = 0;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -36,25 +34,43 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _onScoreTapped(int score) {
+    setState(() {
+      _selectedScore = score;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 80,
         backgroundColor: Colors.grey,
         title: Row(
           children: [
-            Icon(Icons.home, size: 50),
+            FloatingActionButton(
+                backgroundColor: Colors.grey,
+                onPressed: () {},
+                child: Icon(
+                  Icons.home,
+                  size: 50,
+                )),
             SizedBox(width: 20),
-            Icon(Icons.search, size: 50),
-            Text(
-                "검색",
+            FloatingActionButton(
+                backgroundColor: Colors.grey,
+                onPressed: () {},
+                child: Icon(
+                  Icons.search,
+                  size: 50,
+                )),
+            Container(
+              margin: EdgeInsets.all(5.0),
+            ),
+            Text("검색",
                 style: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
-                )
-            ),
+                )),
           ],
         ),
       ),
@@ -71,16 +87,15 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FloatingActionButton(
-                  backgroundColor: Colors.orange,
+                    backgroundColor: Colors.orange,
                     onPressed: () {},
                     child: Icon(
-                        Icons.add,
-                        color: Colors.black,
-                    )
-                ),
+                      Icons.add,
+                      color: Colors.black,
+                    )),
                 SizedBox(width: 15),
                 Text(
-                    '맛집 추가!',
+                  '맛집 추가!',
                   style: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
@@ -93,17 +108,142 @@ class _MyHomePageState extends State<MyHomePage> {
               margin: EdgeInsets.all(20.0),
             ),
 
-            Container(
-                height: 1,
-                width: double.maxFinite,
-                color: Colors.grey
+            Container(height: 1, width: double.maxFinite, color: Colors.grey),
+
+            Opacity(
+              opacity: 0.3,
+              child: Container(
+                margin: EdgeInsets.all(30.0),
+                color: Colors.grey,
+                width: 500.0,
+                height: 500.0,
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    //Padding(padding: EdgeInsets.only(top: 300)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          color: Colors.white,
+                          height: 50,
+                          width: 150,
+                          margin: EdgeInsets.all(10.0),
+                          child: Text(
+                            "가게 이름",
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        FloatingActionButton(
+                            backgroundColor: Colors.grey,
+                            onPressed: () {},
+                            child: Icon(
+                              Icons.arrow_drop_down_circle,
+                              color: Colors.black,
+                              size: 50,
+                            )),
+                      ],
+                    ),
+                    Container(
+                      color: Colors.white,
+                      height: 50,
+                      width: 200,
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                        "날짜",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      height: 50,
+                      width: 400,
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                        "위치",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      height: 50,
+                      width: 500,
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                        "메모",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      margin: EdgeInsets.all(30.0),
+                    ),
+
+                    BottomNavigationBar(
+                      items: <BottomNavigationBarItem>[
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.add),
+                          label: '1',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.add),
+                          label: '2',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.add),
+                          label: '3',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.add),
+                          label: '4',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.add),
+                          label: '5',
+                        ),
+                      ],
+                      iconSize: 40,
+                      currentIndex: _selectedScore,
+                      unselectedItemColor: Colors.blue,
+                      selectedItemColor: Colors.orange,
+                      selectedFontSize: 30,
+                      onTap: _onScoreTapped,
+                    ),
+
+                    // Row(
+                    //   children: [
+                    //     Icon(Icons.looks_one, size: 50),
+                    //     SizedBox(width: 20),
+                    //     Icon(Icons.search, size: 50),
+                    //     Text(
+                    //         "검색",
+                    //         style: TextStyle(
+                    //           fontSize: 25,
+                    //           fontWeight: FontWeight.bold,
+                    //         )
+                    //     ),
+                    //   ],
+                    // ),
+                  ],
+                ),
+              ),
             ),
-
-
           ],
         ),
       ),
-
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
