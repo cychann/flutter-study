971102bce64882a286f43f3893b577e01a7d5fd2
cychann <skcbdlhc129@naver.com>
2021-06-09 22:08:35 +0900

    기본 틀 구현

--- food_store/lib/main.dart
+++ food_store/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -21,182 +19,231 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
+  int bottomSelectedIndex = 0;
 
-  void _onItemTapped(int index) {
+  List<BottomNavigationBarItem> buildBottomNavBarItems() {
+    return [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.food_bank),
+        label: 'Food',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.local_drink),
+        label: 'Cafe',
+      ),
+    ];
+  }
+
+  PageController pageController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
+
+  Widget buildPageView() {
+    return PageView(
+      controller: pageController,
+      onPageChanged: (index) {
+        pageChanged(index);
+      },
+      children: <Widget>[
+        Food(),
+        Cafe(),
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void pageChanged(int index) {
     setState(() {
-      _selectedIndex = index;
+      bottomSelectedIndex = index;
     });
   }
 
-  PageController _controller = PageController(
-    initialPage: 0,
-  );
+  void bottomTapped(int index) {
+    setState(() {
+      bottomSelectedIndex = index;
+      pageController.animateToPage(index,
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _controller,
+      body: buildPageView(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: bottomSelectedIndex,
+        onTap: (index) {
+          bottomTapped(index);
+        },
+        items: buildBottomNavBarItems(),
+      ),
+    );
+  }
+}
+
+class Food extends StatefulWidget {
+  @override
+  _FoodState createState() => _FoodState();
+}
+
+class _FoodState extends State<Food> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          // margin을 넣기 위한 컨테이너 박스
           Container(
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // margin을 넣기 위한 컨테이너 박스
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FloatingActionButton(
-                        backgroundColor: Colors.orange,
-                        onPressed: () {},
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.black,
-                        )),
-                    SizedBox(width: 15),
-                    Text(
-                      '맛집 추가!',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                // margin을 넣기 위한 컨테이너 박스
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                ),
-
-                Container(
-                    height: 1, width: double.maxFinite, color: Colors.grey),
-
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                ),
-
-                CarouselSlider(
-                  options: CarouselOptions(
-                    height: 400.0,
-                    autoPlay: true,
-                    enlargeCenterPage: true,
-                    viewportFraction: 0.5,
-                    aspectRatio: 2.0,
-                    initialPage: 0,
-                  ),
-                  items: [1, 2, 3, 4, 5].map((i) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(color: Colors.amber),
-                            child: Text(
-                              '맛집 $i',
-                              style: TextStyle(fontSize: 16.0),
-                            ));
-                      },
-                    );
-                  }).toList(),
-                )
-              ],
-            ),
+            margin: EdgeInsets.all(20.0),
           ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                  backgroundColor: Colors.orange,
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  )),
+              SizedBox(width: 15),
+              Text(
+                '맛집 추가!',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          // margin을 넣기 위한 컨테이너 박스
           Container(
-            color: Colors.green,
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // margin을 넣기 위한 컨테이너 박스
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FloatingActionButton(
-                        backgroundColor: Colors.orange,
-                        onPressed: () {},
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.black,
-                        )),
-                    SizedBox(width: 15),
-                    Text(
-                      '카페 추가!',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                // margin을 넣기 위한 컨테이너 박스
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                ),
-
-                Container(
-                    height: 1, width: double.maxFinite, color: Colors.grey),
-
-                Container(
-                  margin: EdgeInsets.all(20.0),
-                ),
-
-                CarouselSlider(
-                  options: CarouselOptions(
-                    height: 400.0,
-                    autoPlay: true,
-                    enlargeCenterPage: true,
-                    viewportFraction: 0.5,
-                    aspectRatio: 2.0,
-                    initialPage: 0,
-                  ),
-                  items: [1, 2, 3, 4, 5].map((i) {
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(color: Colors.amber),
-                            child: Text(
-                              '카페 $i',
-                              style: TextStyle(fontSize: 16.0),
-                            ));
-                      },
-                    );
-                  }).toList(),
-                )
-              ],
-            ),
+            margin: EdgeInsets.all(20.0),
           ),
+
+          Container(height: 1, width: double.maxFinite, color: Colors.grey),
+
+          Container(
+            margin: EdgeInsets.all(50.0),
+          ),
+
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 400.0,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              viewportFraction: 0.5,
+              aspectRatio: 2.0,
+              initialPage: 0,
+            ),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(color: Color(0xFFE3F2FD)),
+                      child: Text(
+                        '맛집 $i',
+                        style: TextStyle(fontSize: 16.0),
+                      ));
+                },
+              );
+            }).toList(),
+          )
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.food_bank),
-            label: 'Food',
+    );
+  }
+}
+
+class Cafe extends StatefulWidget {
+  @override
+  _CafeState createState() => _CafeState();
+}
+
+class _CafeState extends State<Cafe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          // margin을 넣기 위한 컨테이너 박스
+          Container(
+            margin: EdgeInsets.all(20.0),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_drink),
-            label: 'Cafe',
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                  backgroundColor: Colors.orange,
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  )),
+              SizedBox(width: 15),
+              Text(
+                '카페 추가!',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
+          // margin을 넣기 위한 컨테이너 박스
+          Container(
+            margin: EdgeInsets.all(20.0),
+          ),
+
+          Container(height: 1, width: double.maxFinite, color: Colors.grey),
+
+          Container(
+            margin: EdgeInsets.all(50.0),
+          ),
+
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 400.0,
+              autoPlay: true,
+              enlargeCenterPage: true,
+              viewportFraction: 0.5,
+              aspectRatio: 2.0,
+              initialPage: 0,
+            ),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(color: Color(0xFFD6D6D6)),
+                      child: Text(
+                        '카페 $i',
+                        style: TextStyle(fontSize: 16.0),
+                      ));
+                },
+              );
+            }).toList(),
+          )
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
       ),
     );
   }
